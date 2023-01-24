2fe927e95321cb67d65554d3506d65668263c480
Simon Bourne <simonbourne@gmail.com>
2023-01-24 18:52:39 +0000

    Spawn server and client in tokio

--- packages/integration-tests/src/main.rs
+++ packages/integration-tests/src/main.rs
@@ -1,11 +1,13 @@
-use std::{thread, time::Duration, net::SocketAddr};
+use std::{net::SocketAddr, thread, time::Duration};
 
 use async_trait::async_trait;
 use axum::{Router, Server};
+use hyper::server::{self, conn::AddrIncoming};
 use rpc::{RemoteFn, RpcClient};
 use rpc_axum::handle_rpc;
 use rpc_reqwest::Connection;
 use serde::{Deserialize, Serialize};
+use tokio::spawn;
 
 #[derive(Serialize, Deserialize)]
 struct Add(i32, i32);
@@ -19,28 +21,24 @@ impl RemoteFn for Add {
     }
 }
 
-fn main() {
-    thread::scope(|scope| {
-        scope.spawn(server);
-        scope.spawn(|| {
-            thread::sleep(Duration::from_secs(1));
-            client();
-        });
-    });
+#[tokio::main]
+async fn main() {
+    let listener = Server::bind(&SocketAddr::from(([0, 0, 0, 0], 9090)));
+
+    let client = spawn(client());
+    let server = spawn(server(listener));
+
+    assert_eq!(3, client.await.unwrap().unwrap());
+    server.abort();
 }
 
-#[tokio::main]
-async fn client() {
+async fn client() -> Result<i32, rpc_reqwest::Error> {
     let client = Connection::new(&reqwest::Client::new(), "http://127.0.0.1:9090/api/add");
-    let result = client.call(&Add(1, 2)).await;
-    println!("{:?}", result);
+    client.call(&Add(1, 2)).await
 }
 
-#[tokio::main]
-async fn server() {
+async fn server(listener: server::Builder<AddrIncoming>) {
+    thread::sleep(Duration::from_secs(10));
     let app = Router::new().route("/api/add", handle_rpc::<Add>());
-    Server::bind(&SocketAddr::from(([0, 0, 0, 0], 9090)))
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    listener.serve(app.into_make_service()).await.unwrap();
 }
