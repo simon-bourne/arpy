7ea0df09e5e2f811b392fe4aefc3f157736516f6
Simon Bourne <simonbourne@gmail.com>
2023-01-25 18:07:33 +0000

    Add websockets tests

--- packages/reqwasm/tests/external_server.rs
+++ packages/reqwasm/tests/external_server.rs
@@ -1,6 +1,7 @@
-// These tests require `test-servers` to be running
+//! These tests require `test-server` to be running
+use reqwasm::websocket::futures::WebSocket;
 use rpc::RpcClient;
-use rpc_reqwasm::http;
+use rpc_reqwasm::{http, websocket};
 use rpc_test_data::{Add, PORT};
 use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
 
@@ -8,7 +9,15 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 async fn http_client() {
-    let mut request = http::Connection::new(&format!("http://127.0.0.1:{PORT}/api/add"));
+    let mut request = http::Connection::new(&format!("http://127.0.0.1:{PORT}/http/add"));
+
+    assert_eq!(3, request.call(&Add(1, 2)).await.unwrap());
+}
+
+#[wasm_bindgen_test]
+async fn websocket_client() {
+    let ws = WebSocket::open(&format!("ws://127.0.0.1:{PORT}/websocket/add")).unwrap();
+    let mut request = websocket::Connection::new(ws);
 
     assert_eq!(3, request.call(&Add(1, 2)).await.unwrap());
 }
--- packages/test-servers/src/main.rs
+++ packages/test-servers/src/main.rs
@@ -1,15 +1,21 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use axum::{Router, Server};
-use rpc_axum::http::handle_rpc;
+use rpc_axum::{http, websocket};
 use rpc_test_data::{Add, TryMultiply, PORT};
 use tower_http::cors::CorsLayer;
 
+// TODO: HTTP and WebSocket servers
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/api/add", handle_rpc::<Add>())
-        .route("/api/multiply", handle_rpc::<TryMultiply>())
+        .route("/http/add", http::handle_rpc::<Add>())
+        .route("/http/multiply", http::handle_rpc::<TryMultiply>())
+        .route("/websocket/add", websocket::handle_rpc::<Add>())
+        .route(
+            "/websocket/multiply",
+            websocket::handle_rpc::<TryMultiply>(),
+        )
         .layer(CorsLayer::permissive());
 
     Server::bind(&SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), PORT))
