b248d585441bdd5f236b1efc44e3b7a2f4fd157a
Simon Bourne <simonbourne@gmail.com>
2023-01-25 10:34:44 +0000

    Add cors layer to test server

--- packages/test-servers/src/main.rs
+++ packages/test-servers/src/main.rs
@@ -3,12 +3,14 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use axum::{Router, Server};
 use rpc_axum::handle_rpc;
 use rpc_test_data::{Add, TryMultiply, PORT};
+use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/api/add", handle_rpc::<Add>())
-        .route("/api/multiply", handle_rpc::<TryMultiply>());
+        .route("/api/multiply", handle_rpc::<TryMultiply>())
+        .layer(CorsLayer::permissive());
 
     Server::bind(&SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), PORT))
         .serve(app.into_make_service())
