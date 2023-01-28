155aa4c0d1f24d0dee5d11430f51ac9ec19231df
Simon Bourne <simonbourne@gmail.com>
2023-01-28 15:17:17 +0000

    Better server error handling

--- packages/server/src/websocket.rs
+++ packages/server/src/websocket.rs
@@ -1,8 +1,9 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, io, result, sync::Arc};
 
-use anyhow::bail;
 use arpy::{FnRemote, FnRemoteBody};
+use ciborium::de;
 use futures::future::BoxFuture;
+use thiserror::Error;
 
 #[derive(Default)]
 pub struct WebSocketRouter(HashMap<Id, RpcHandler>);
@@ -27,12 +28,13 @@ impl WebSocketRouter {
         self
     }
 
-    async fn run<F, T>(f: Arc<F>, input: Vec<u8>) -> anyhow::Result<Vec<u8>>
+    async fn run<F, T>(f: Arc<F>, input: Vec<u8>) -> Result<Vec<u8>>
     where
         F: for<'a> FnRemoteBody<'a, T> + Send + Sync + 'static,
         T: FnRemote + Send + Sync + 'static,
     {
-        let args: T = ciborium::de::from_reader(input.as_slice())?;
+        let args: T =
+            ciborium::de::from_reader(input.as_slice()).map_err(Error::Deserialization)?;
         let result = f.run(&args).await;
         let mut body = Vec::new();
         ciborium::ser::into_writer(&result, &mut body).unwrap();
@@ -47,15 +49,24 @@ impl WebSocketHandler {
         Self(router.0)
     }
 
-    // TODO: Don't use anyhow for result here.
-    pub async fn handle_msg(&self, id: &[u8], params: Vec<u8>) -> anyhow::Result<Vec<u8>> {
+    pub async fn handle_msg(&self, id: &[u8], params: Vec<u8>) -> Result<Vec<u8>> {
         let Some(function) = self.0.get(id)
-        else { bail!("Function not found") };
+        else { return Err(Error::FunctionNotFound) };
 
         function(params).await
     }
 }
 
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("Function not found")]
+    FunctionNotFound,
+    #[error("Deserialization: {0}")]
+    Deserialization(de::Error<io::Error>),
+}
+
+pub type Result<T> = result::Result<T, Error>;
+
 type Id = Vec<u8>;
 type RpcHandler =
-    Box<dyn Fn(Vec<u8>) -> BoxFuture<'static, anyhow::Result<Vec<u8>>> + Send + Sync + 'static>;
+    Box<dyn Fn(Vec<u8>) -> BoxFuture<'static, Result<Vec<u8>>> + Send + Sync + 'static>;
