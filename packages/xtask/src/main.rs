e1ec55153f1e619db8fbc957b2789a78b148f64f
Simon Bourne <simonbourne@gmail.com>
2023-10-20 19:25:38 +0100

    CI: Install wasm-pack

--- packages/xtask/src/main.rs
+++ packages/xtask/src/main.rs
@@ -1,12 +1,28 @@
-use xtask_base::{ci::CI, generate_open_source_files, CommonCmds};
+use xtask_base::{
+    ci::{Tasks, CI},
+    generate_open_source_files,
+    github::actions::{install, rust_toolchain, Platform},
+    CommonCmds,
+};
 
 fn main() {
-    // TODO: Use `CI::standard_workflow()` once release mode tests are working.
-    CommonCmds::run(ci(), |check| generate_open_source_files(2021, check))
+    let codegen = |check| generate_open_source_files(2021, check);
+    CommonCmds::run(ci(), codegen)
 }
 
 fn ci() -> CI {
-    CI::new()
-        .standard_tests("1.73")
-        .standard_lints("nightly-2023-10-14", "0.1.43")
+    let mut workflow = CI::new();
+    for platform in Platform::latest() {
+        workflow.add_job(
+            Tasks::new(
+                "tests",
+                platform,
+                rust_toolchain("1.73").minimal().default().clippy().wasm(),
+            )
+            .step(install("wasm-pack", "0.12.1"))
+            .tests(),
+        );
+    }
+
+    workflow.standard_lints("nightly-2023-10-14", "0.1.43")
 }
